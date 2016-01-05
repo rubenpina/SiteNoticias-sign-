33be98a5c4fb625e79186cfe4fff6a12f081caa9
rubenpina <rubengomespina@gmail.com>
2016-01-05 12:26:48 +0000

    Ficheiros Log in e Sign Up

---
+++ signin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sign Up</title>
+        <link rel="stylesheet" href="css/signup.css">
+    </head>
+    <body>
+        <div class="row social">
+            <div class="face_col">
+                <a href="">
+                    <ul class="face">
+                       <i class="fb"> </i>
+                        <li>Conectar com Facebook</li>
+                    </ul>
+                </a>
+            </div>
+            <div class="twt_col">
+                <a href="">
+                    <ul class="twt">
+                        <i class="tt"> </i>
+                        <li>Conectar com Twitter</li>
+                    </ul>
+                </a>
+            </div>
+        </div>
+        <form action="">
+            <div class="row user_log">
+                <input type="email" placeholder="Endereço eletrónico">
+            </div>
+            <div class="row user_pass">
+                <input type="password" placeholder="Palavra-passe"><a href="#">Esqueceu palavra-passe?</a>
+            </div>
+            <div class="row sub">
+                <a href="#"><input type="submit" value="Entrar"></a>
+            </div>
+        </form>
+        <div class="row">
+            <a class="regi" href="signup.php">Não tem conta? Crie já uma!</a>
+        </div>
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sign Up</title>
+        <link rel="stylesheet" href="css/signup.css">
+    </head>
+    <body>
+        <div class="row social">
+            <div class="face_col">
+                <a href="">
+                    <ul class="face">
+                       <i class="fb"> </i>
+                        <li>Conectar com Facebook</li>
+                    </ul>
+                </a>
+            </div>
+            <div class="twt_col">
+                <a href="">
+                    <ul class="twt">
+                        <i class="tt"> </i>
+                        <li>Conectar com Twitter</li>
+                    </ul>
+                </a>
+            </div>
+        </div>
+        <form action="">
+            <div class="row user_name">
+                <input type="text" placeholder="Nome">
+                <input type="text" placeholder="Apelido">
+            </div>
+            <div class="row user_log">
+                <input type="email" placeholder="Endereço eletrónico">
+            </div>
+            <div class="row user_pass">
+                <input type="password" placeholder="Palavra-passe">
+            </div>
+            <div class="row sub">
+                <p>Ao criar conta estará a aceitar os <a href="#">termos de privacidade e condições</a></p>
+                <a href="#"><input type="submit" value="Registar"></a>
+            </div>
+        </form>
+    </body>
+</html>
